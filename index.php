bd9769b8cf592b3ec3519f458e9e3d638dafd3f4
Lucas Mendonça <lucasmm0908@gmail.com>
2023-05-17 11:13:25 -0300

    primeiro commit
    subindo a core do site

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Celke</title>
+    </head>
+    <body>
+        <?php
+            require './vendor/autoload.php';
+
+            $url = new Core\ConfigController();
+        ?>
+    </body>
+</html>
